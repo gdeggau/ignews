b8c026f314025e9b69479093ee6096b17a13c9e6
gabrieldeggau <gabriel.deggau@premiersoft.net>
2021-12-24 15:56:01 -0300

    test: ensure SignInButton component render correctly when user is authenticated

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -9,9 +9,26 @@ jest.mock("next-auth/client");
 describe("SignInButton component", () => {
   it("renders correctly when user is not authenticated", () => {
     const useSessionMocked = mocked(useSession);
-    useSessionMocked.mockReturnValue([null, false]);
+    useSessionMocked.mockReturnValueOnce([null, false]);
 
     render(<SignInButton />);
     expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
+  });
+
+  it("renders correctly when user is authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+    useSessionMocked.mockReturnValue([
+      {
+        user: {
+          name: "John Doe",
+          email: "johndoe@email.com",
+        },
+        expires: "fake-expires",
+      },
+      false,
+    ]);
+
+    render(<SignInButton />);
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
   });
 });
