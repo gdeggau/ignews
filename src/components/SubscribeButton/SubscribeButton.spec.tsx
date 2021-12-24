2eedd5a2ca0081194ba9726c507f335c4b97b0e4
gabrieldeggau <gabriel.deggau@premiersoft.net>
2021-12-24 16:14:53 -0300

    test: ensure SubscribeButton component redirects user to sign in when not authenticated

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,4 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { signIn } from "next-auth/client";
+import { mocked } from "ts-jest/utils";
 import { SubscribeButton } from ".";
 
 jest.mock("next-auth/client", () => {
@@ -6,6 +8,7 @@ jest.mock("next-auth/client", () => {
     useSession() {
       return [null, false];
     },
+    signIn: jest.fn(),
   };
 });
 
@@ -13,5 +16,13 @@ describe("SubscribeButton component", () => {
   it("renders correctly", () => {
     render(<SubscribeButton />);
     expect(screen.getByText("Subscribe now")).toBeInTheDocument();
+  });
+
+  it("redirects user to sign in when not authenticated", () => {
+    const signInMocked = mocked(signIn);
+    render(<SubscribeButton />);
+    const subscribeButton = screen.getByText("Subscribe now");
+    fireEvent.click(subscribeButton);
+    expect(signInMocked).toHaveBeenCalled();
   });
 });
