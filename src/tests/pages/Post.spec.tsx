52c3f293beffdf4f9146f1d6dc20b47469893e5b
gabrieldeggau <gabriel.deggau@premiersoft.net>
2021-12-25 16:03:16 -0300

    test: ensure Post page redirects user if no subscription is found

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { getSession } from "next-auth/client";
 import { mocked } from "ts-jest/utils";
 import PostPage, { getServerSideProps, Post } from "../../pages/posts/[slug]";
 import { getPrismicClient } from "../../services/prismic";
 
 jest.mock("../../services/prismic");
+jest.mock("next-auth/client");
 
 const post: Post = {
   slug: "my-new-post",
@@ -17,5 +19,24 @@ describe("Post page", () => {
     render(<PostPage post={post} />);
     expect(screen.getByText("My new post")).toBeInTheDocument();
     expect(screen.getByText("Post excerpt")).toBeInTheDocument();
+  });
+
+  it("redirects user if no subscription is found", async () => {
+    const getSessionMocked = mocked(getSession);
+    getSessionMocked.mockResolvedValueOnce(null);
+
+    const response = await getServerSideProps({
+      params: {
+        slug: "my-new-post",
+      },
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        redirect: expect.objectContaining({
+          destination: "/posts/preview/my-new-post",
+        }),
+      })
+    );
   });
 });
