da5269c799fdbe5d19bb28efd28128e66ab5673a
gabrieldeggau <gabriel.deggau@premiersoft.net>
2021-12-25 18:45:40 -0300

    test: ensure PostPreview page loads initial data

--- src/tests/pages/PostPreview.spec.tsx
+++ src/tests/pages/PostPreview.spec.tsx
@@ -2,7 +2,11 @@ import { render, screen } from "@testing-library/react";
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import { mocked } from "ts-jest/utils";
-import PostPreview, { Post } from "../../pages/posts/preview/[slug]";
+import PostPreview, {
+  getStaticProps,
+  Post,
+} from "../../pages/posts/preview/[slug]";
+import { getPrismicClient } from "../../services/prismic";
 
 jest.mock("../../services/prismic");
 jest.mock("next-auth/client");
@@ -42,5 +46,48 @@ describe("Post preview page", () => {
 
     render(<PostPreview post={post} />);
     expect(pushMock).toHaveBeenCalledWith("/posts/my-new-post");
+  });
+
+  it("loads initial data", async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            {
+              type: "heading",
+              text: "My new post",
+            },
+          ],
+          content: [
+            {
+              type: "paragraph",
+              text: "Post content",
+            },
+          ],
+        },
+        last_publication_date: "04-01-2021",
+      }),
+    } as any);
+
+    const response = await getStaticProps({
+      params: {
+        slug: "my-new-post",
+      },
+    });
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: "my-new-post",
+            title: "My new post",
+            content: "<p>Post content</p>",
+            updatedAt: "01 de abril de 2021",
+          },
+        },
+      })
+    );
   });
 });
