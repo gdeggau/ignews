53774b601b1f49bb1da516dd9d7ec835792c8147
gabrieldeggau <gabriel.deggau@premiersoft.net>
2021-12-25 16:16:29 -0300

    test: ensure Post page loads initial data

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -39,4 +39,51 @@ describe("Post page", () => {
       })
     );
   });
+
+  it("loads initial data", async () => {
+    const getSessionMocked = mocked(getSession);
+    const getPrismicClientMocked = mocked(getPrismicClient);
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: "fake-subscription-id",
+    });
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            {
+              type: "heading",
+              text: "My new post",
+            },
+          ],
+          content: [
+            {
+              type: "paragraph",
+              text: "Post content",
+            },
+          ],
+        },
+        last_publication_date: "04-01-2021",
+      }),
+    } as any);
+
+    const response = await getServerSideProps({
+      params: {
+        slug: "my-new-post",
+      },
+    } as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: "my-new-post",
+            title: "My new post",
+            content: "<p>Post content</p>",
+            updatedAt: "01 de abril de 2021",
+          },
+        },
+      })
+    );
+  });
 });
