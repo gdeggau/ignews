d9b73cf3fec7baf13c6a85fcf93ebdae7918f65b
gabrieldeggau <gabriel.deggau@premiersoft.net>
2021-12-25 18:43:40 -0300

    test: ensure PostPreview redirects user to full post when user ir subscribed

--- src/tests/pages/PostPreview.spec.tsx
+++ src/tests/pages/PostPreview.spec.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import { mocked } from "ts-jest/utils";
 import PostPreview, { Post } from "../../pages/posts/preview/[slug]";
 
 jest.mock("../../services/prismic");
 jest.mock("next-auth/client");
+jest.mock("next/router");
 
 const post: Post = {
   slug: "my-new-post",
@@ -21,5 +23,24 @@ describe("Post preview page", () => {
     expect(screen.getByText("My new post")).toBeInTheDocument();
     expect(screen.getByText("Post excerpt")).toBeInTheDocument();
     expect(screen.getByText("Wanna continue reading?")).toBeInTheDocument();
+  });
+
+  it("redirects user to full post when user ir subscribed", () => {
+    const useSessionMocked = mocked(useSession);
+    const useRouterMocked = mocked(useRouter);
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        activeSubscription: "fake-subscription-id",
+      },
+      false,
+    ]);
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
+    render(<PostPreview post={post} />);
+    expect(pushMock).toHaveBeenCalledWith("/posts/my-new-post");
   });
 });
